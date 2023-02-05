a04aec1fc213c0a6f51356fde37f53a3825d057b
SerhiiMashinskiy <example@gmail.com>
2023-02-05 11:37:00 +0200

    first commit

---
+++ work/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// alert("hello");
+
+// const result = confirm("Are yue here");
+  
+// console.log(result);
+
+// const answer = +prompt("Vam est 18", "18");
+// console.log(answer + 5);
+
+const answers = [];
+answers[0] = prompt("Waht your name", "");
+answers[1] = prompt("Waht your fname", "");
+answers[2] = prompt("Age", "");
+
+document.write(answers);
+
